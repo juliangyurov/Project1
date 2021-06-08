652ac11a92bfe97ffc67ead3d296cfa0c5d612b4
Yulian Gyurov <yg.atanasoff@gmail.com>
2021-06-08 22:23:10 +0300

    add file with testing user data

---
+++ TestingUserCredentials.js
@@ -0,0 +1,6 @@
+// check localStorage entries
+console.log(getUser("emarinova@gmail.com"));    //Em1234567*
+console.log(getUser("julian.gyuroff@gmail.com"));
+console.log(getUser("john.d@example.org"));     //Jd1234567*
+console.log(getUser("anna.s@example.org"));     //1
+console.log(getUser("peter.j@example.org"));
